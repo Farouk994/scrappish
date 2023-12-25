a0ceaa584e342ffc0891f717ac08d7195963dc93
hs-fkisuule <farouk.kisuule@homestars.com>
2023-12-25 14:42:36 -0500

    nav setup

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+/** @format */
+
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href='/' className='flex items-center gap-1'>
+          <Image
+            src='/assets/icons/logo.svg'
+            alt='logo'
+            width={27}
+            height={27}
+          />
+          <p className='nav-logo'>
+            Scrapp<span className='text-primary'>ish</span>
+          </p>
+        </Link>
+        <div className='flex items-center gap-5'>
+          {navIcons.map((icon, index) => (
+            <Image
+              key={index}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className='object-contain'
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
