8d8ee2170738649640dc75d373be5cfaa7772a49
hs-fkisuule <farouk.kisuule@homestars.com>
2023-12-30 13:30:53 -0500

    deploy fix

--- client/app/api/cron/route.ts
+++ client/app/api/cron/route.ts
@@ -16,71 +16,73 @@ export const maxDuration = 5; // 5 minutes
 export const dynamic = "force-dynamic"; // force dynamic rendering
 export const revalidate = 0;
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
     connectDb();
-    const products = await Product.find({});
-    if (!products) {
-      throw new Error("No products found");
-    }
 
-    // 1.SCRAP latest product details and update db
+    const products = await Product.find({});
+
+    if (!products) throw new Error("No product fetched");
+
+    // ======================== 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
     const updatedProducts = await Promise.all(
       products.map(async (currentProduct) => {
+        // Scrape product
         const scrapedProduct = await scrapAmazonProduct(currentProduct.url);
-        if (!scrapedProduct) throw new Error("Error in scraping product");
 
-        // update price history
+        if (!scrapedProduct) return;
+
         const updatedPriceHistory = [
           ...currentProduct.priceHistory,
-          { price: scrapedProduct.currentPrice },
+          {
+            price: scrapedProduct.currentPrice,
+          },
         ];
 
         const product = {
           ...scrapedProduct,
           priceHistory: updatedPriceHistory,
           lowestPrice: getLowestPrice(updatedPriceHistory),
-          getHighestPrice: getHighestPrice(updatedPriceHistory),
+          highestPrice: getHighestPrice(updatedPriceHistory),
           averagePrice: getAveragePrice(updatedPriceHistory),
         };
 
+        // Update Products in DB
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: product.url },
+          {
+            url: product.url,
+          },
           product
         );
 
-        // check each product status and send email accordingly
+        // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
         const emailNotifType = getEmailNotifType(
           scrapedProduct,
           currentProduct
         );
+
         if (emailNotifType && updatedProduct.users.length > 0) {
-          // send email
           const productInfo = {
             title: updatedProduct.title,
             url: updatedProduct.url,
           };
-
-          const emailContent = await generateEmailBody(
-            productInfo,
-            emailNotifType
-          );
-
-          const userEmails = updatedProduct.users.map(
-            (user: any) => user.email
-          );
-
-          // send email
+          // Construct emailContent
+          const emailContent = await generateEmailBody(productInfo, emailNotifType);
+          // Get array of user emails
+          const userEmails = updatedProduct.users.map((user: any) => user.email);
+          // Send email notification
           await sendEmail(emailContent, userEmails);
         }
+
         return updatedProduct;
       })
     );
+
     return NextResponse.json({
-      message: "OK",
+      message: "Ok",
       data: updatedProducts,
     });
   } catch (error: any) {
-    throw new Error(`Error in GET job: ${error}`);
+    throw new Error(`Failed to get all products: ${error.message}`);
   }
 }
